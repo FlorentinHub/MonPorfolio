ef929bb2af528cafa0ce6cb5e7feb74fd9676129
Ftoupet <2132122@cmontmorency.qc.ca>
2023-10-26 23:18:03 -0400

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,3 +4,16 @@
                 <p>{{ __('auth.logged_in_as', ['name' => auth()->user()->name]) }}</p>
             @endif
         </footer>
+
+        <style>
+            footer {
+                background-color: #111;
+                text-align: center;
+                padding: 20px 0;
+            }
+
+            footer p {
+                font-size: 20px;
+                font-weight: bold;
+            }
+        </style>
